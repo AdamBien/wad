f596ff17f53a93cdeec6111a8989037b54d98892
Adam Bien <abien@adam-bien.com>
2018-12-09 19:59:44 +0100

    ThinWAR name computed from current folder

--- src/main/java/wad/App.java
+++ src/main/java/wad/App.java
@@ -1,6 +1,7 @@
 
 package wad;
 
+import com.airhacks.wad.watch.boundary.WADFlow;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,7 +13,19 @@ import java.nio.file.Paths;
 public class App {
 
     public static void main(String[] args) throws IOException {
-        Path dir = Paths.get("./src/main/java");
+        if (args.length != 1) {
+            System.out.println("Invoke with java -jar wad.jar [DEPLOYMENT_DIR]");
+            System.exit(-1);
+        }
 
+        Path deploymentDir = Paths.get(args[0]);
+        Path sourceCodeDir = Paths.get("./src/main/java");
+
+        Path currentPath = Paths.get("").toAbsolutePath();
+        Path currentDirectory = currentPath.getFileName();
+
+        Path thinWAR = Paths.get("target", currentDirectory + ".war");
+
+        WADFlow.wad(sourceCodeDir, thinWAR, deploymentDir);
     }
 }
