818e0a0eff1ae51e130b54a161edb7db115e4674
Adam Bien <abien@adam-bien.com>
2018-12-09 19:04:14 +0100

    watch service implemented

---
+++ src/main/java/com/airhacks/wad/watch/boundary/FolderWatch.java
@@ -0,0 +1,47 @@
+
+package com.airhacks.wad.watch.boundary;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public interface FolderWatch {
+
+
+    public static void listenForChanges(Path dir, Runnable listener) throws IOException {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        WatchService service = FileSystems.getDefault().newWatchService();
+        Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                    throws IOException {
+                dir.register(service, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        while (true) {
+            try {
+                service.take();
+                System.out.println(".");
+                pool.submit(listener);
+            } catch (InterruptedException x) {
+                return;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/wad/App.java
@@ -0,0 +1,18 @@
+
+package wad;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        Path dir = Paths.get("./src/main/java");
+
+    }
+}
