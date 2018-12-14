71b541384776b9b402d6e4bd1d1e0e8ad9d8c750
Adam Bien <abien@adam-bien.com>
2018-12-14 10:44:37 +0100

    number of concurrent threads is implemented

--- src/main/java/com/airhacks/wad/watch/control/Builder.java
+++ src/main/java/com/airhacks/wad/watch/control/Builder.java
@@ -21,6 +21,9 @@ public class Builder {
 
     public Builder() {
         this.invoker = new DefaultInvoker();
+        this.invoker.setLogger(new SilentLogger());
+        this.invoker.setOutputHandler((line) -> {
+        });
         List<String> goals = Arrays.asList("clean", "package");
         Properties properties = new Properties();
         properties.put("maven.test.skip", String.valueOf(true));
@@ -29,6 +32,8 @@ public class Builder {
         this.request.setGoals(goals);
         this.request.setBatchMode(true);
         this.request.setProperties(properties);
+        this.request.setThreads(System.getProperty("threads", "1"));
+        this.request.setShowErrors(true);
     }
 
     public InvocationResult build() throws MavenInvocationException {
