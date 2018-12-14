654b0b94b8eac0e629961bef9da25c65c9d398c9
Adam Bien <abien@adam-bien.com>
2018-12-14 10:44:58 +0100

    superfluous information removed from log

--- src/main/java/com/airhacks/wad/watch/control/Copier.java
+++ src/main/java/com/airhacks/wad/watch/control/Copier.java
@@ -12,9 +12,19 @@ import java.nio.file.StandardCopyOption;
  */
 public interface Copier {
 
+    static String shortenForDisplay(Path path, int maxLength) {
+        String message = path.toString();
+        int length = message.length();
+        if (length > maxLength) {
+            return "(...)" + message.substring(length - maxLength);
+        } else {
+            return message;
+        }
+    }
+
     public static Path copy(Path from, Path to) throws IOException {
         long kb = Files.size(from) / 1024;
-        System.out.printf("Copying %s (%d kB ThinWAR) to %s \n", from, kb, to);
+        System.out.printf("Copying %dkB ThinWAR to %s \n", kb, shortenForDisplay(to, 40));
         return Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
     }
 
