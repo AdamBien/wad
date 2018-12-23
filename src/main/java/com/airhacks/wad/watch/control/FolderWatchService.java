f7e5636f26da28bc310fc0b16cf09a21d0d2f1b8
Adam Bien <abien@adam-bien.com>
2018-12-23 09:07:14 +0100

    custom change modification implemented

--- src/main/java/com/airhacks/wad/watch/control/FolderWatchService.java
+++ src/main/java/com/airhacks/wad/watch/control/FolderWatchService.java
@@ -2,17 +2,12 @@
 package com.airhacks.wad.watch.control;
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -20,36 +15,46 @@ import java.nio.file.attribute.BasicFileAttributes;
  */
 public interface FolderWatchService {
 
+    static long POLLING_INTERVALL = 500;
 
     public static void listenForChanges(Path dir, Runnable listener) throws IOException {
-        WatchService service = FileSystems.getDefault().newWatchService();
-        Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-                    throws IOException {
-                dir.register(service, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-                return FileVisitResult.CONTINUE;
-            }
-        });
-        while (true) {
-            try {
-                WatchKey key = service.take();
-                key.pollEvents();
-                listener.run();
-                key.reset();
-            } catch (InterruptedException x) {
-                return;
-            }
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        checkForChanges(scheduler, POLLING_INTERVALL, dir, listener);
+    }
+
+    static void checkForChanges(ScheduledExecutorService scheduler, long initialTimeStamp, Path dir, Runnable changeListener) {
+        long initialStamp = getFolderModificationId(dir);
+        System.out.println("Initial timestamp " + initialStamp);
+        boolean changeDetected = false;
+        try {
+            changeDetected = scheduler.schedule(() -> detectModification(dir, initialStamp), POLLING_INTERVALL, TimeUnit.MILLISECONDS).get();
+            System.out.println("Change detected " + changeDetected);
+        } catch (InterruptedException | ExecutionException ex) {
+            throw new IllegalStateException("Scheduler error", ex);
+        }
+        if (changeDetected) {
+            changeListener.run();
+        }
+        checkForChanges(scheduler, getFolderModificationId(dir), dir, changeListener);
+
+    }
+
+    static boolean detectModification(Path dir, long previousStamp) {
+        long currentStamp = getFolderModificationId(dir);
+        return previousStamp != currentStamp;
+    }
+
+    static long getFolderModificationId(Path dir) {
+        try {
+            return Files.list(dir).
+                    mapToLong(FolderWatchService::getFileSize).
+                    sum();
+        } catch (IOException ex) {
+            throw new IllegalStateException("Cannot list files", ex);
         }
     }
 
-    static long listFiles(Path dir) throws IOException {
-        return Files.list(dir).
-                mapToLong(FolderWatchService::getFileTime).
-                sum();
-    }
-
-    static long getFileTime(Path p) {
+    static long getFileSize(Path p) {
         try {
             return Files.size(p);
         } catch (IOException ex) {
