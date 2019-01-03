db4fef622a084bb67e1131e7ca62c45d1b9a6576
Adam Bien <abien@adam-bien.com>
2019-01-03 16:40:18 +0100

    build time statistics exposed

--- src/main/java/com/airhacks/wad/watch/boundary/WADFlow.java
+++ src/main/java/com/airhacks/wad/watch/boundary/WADFlow.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LongSummaryStatistics;
 import java.util.concurrent.atomic.AtomicLong;
 import org.apache.maven.shared.invoker.InvocationResult;
 
@@ -16,14 +17,15 @@ import org.apache.maven.shared.invoker.InvocationResult;
  * @author airhacks.com
  */
 public class WADFlow {
-    private final static List<Long> buildTimes = new ArrayList<>();
+    private final List<Long> buildTimes;
 
-    private final static AtomicLong successCounter = new AtomicLong();
+    private final AtomicLong successCounter = new AtomicLong();
     private final Copier copier;
     private final Builder builder;
 
     public WADFlow(Path dir, Path war, List<Path> deploymentTargets) throws IOException {
         this.builder = new Builder();
+        this.buildTimes = new ArrayList<>();
         this.copier = new Copier(war, deploymentTargets);
         Runnable changeListener = () -> buildAndDeploy(war, deploymentTargets);
         changeListener.run();
@@ -51,6 +53,13 @@ public class WADFlow {
         } catch (Exception ex) {
             System.err.println(ex.getClass().getName() + " " + ex.getMessage());
         }
+    }
+
+    public LongSummaryStatistics buildTimeStatistics() {
+        return this.buildTimes.
+                stream().
+                mapToLong(t -> t).
+                summaryStatistics();
     }
 
 }
